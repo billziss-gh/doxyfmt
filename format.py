ec63c096d46789a1f49f80ca41d3c443cf534270
Bill Zissimopoulos <billziss@navimatics.com>
2020-12-04 01:06:13 -0800

    checkpoint

--- format.py
+++ format.py
@@ -12,35 +12,69 @@ import os
 class format(object):
     def __init__(self, index):
         self.index = index
+        self.language = ""
 
     def title(self, s):
         pass
+    def __title(self, s):
+        if s:
+            self.title(s)
+
+    def name(self, s):
+        pass
+    def __name(self, s):
+        if s:
+            self.name(s)
+
+    def code(self, s):
+        pass
+    def __code(self, s):
+        if s:
+            self.code(s)
 
     def description(self, elem):
         pass
+    def __description(self, elem):
+        if elem["$"]:
+            self.description(elem)
+
+    def param(self, elem):
+        pass
 
     def member(self, elem):
-        for desc in elem["briefdescription%"]:
-            self.description(desc)
-        for desc in elem["detaileddescription%"]:
-            self.description(desc)
+        self.__name(elem["kind$"] + " " + elem["name$"])
+        self.__code(elem["definition$"] + elem["argsstring$"])
+        for parm in elem["param%"]:
+            self.param(parm)
+        self.__description(elem["briefdescription!"])
+        self.__description(elem["detaileddescription!"])
 
     def section(self, elem):
-        for desc in elem["description%"]:
-            self.description(desc)
+        self.__description(elem["description!"])
         for memb in elem["memberdef%"]:
             self.member(memb)
 
     def compound(self, elem):
-        file = elem["location!"]["file@"]
-        if not file or os.path.isabs(file):
-            self.title(elem["compoundname$"])
+        self.language = elem["language@"]
+        if "file" == elem["kind@"]:
+            title = elem["title$"]
+            if title:
+                self.__title(title)
+            else:
+                title = elem["location!"]["file@"]
+                if title and not os.path.isabs(title):
+                    self.__title(title)
+                else:
+                    self.__title(elem["compoundname$"])
         else:
-            self.title(file)
-        for desc in elem["briefdescription%"]:
-            self.description(desc)
-        for desc in elem["detaileddescription%"]:
-            self.description(desc)
+            self.__name(elem["compoundname$"])
+        self.__description(elem["briefdescription!"])
+        self.__description(elem["detaileddescription!"])
+        for incl in elem["innerclass%"]:
+            comp = self.index[incl["refid@"]].element()
+            lang = self.language
+            self.compound(comp)
+            self.language = lang
         for sect in elem["sectiondef%"]:
             self.section(sect)
 
--- formats/markdown.pyt
+++ formats/markdown.pyt
@@ -1,6 +1,14 @@
 from format import *
 
 class markdown(format):
+    def title(self, s):
+        : # ${s}
+    def name(self, s):
+        : **${s}**
+    def code(self, s):
+        : ```${self.language}
+        : ${s}
+        : ```
     def description(self, elem):
         : ${elem}
 
